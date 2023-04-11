f96126a9bc9cf2484145a2cf03abd669bf718b10
minhhau <nguyenminhhau28032003@gamil.com>
2023-04-11 15:20:00 +0700

    video #8 useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,42 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./views/Nav.jsx";
+import { useState } from "react";
 
 function App() {
-  let name = "minh hau";
-  let obj = {
-    name: "hau",
-    study: "react js hook",
+  // state
+  let [name, setName] = useState("hau");
+  let [address, setAddress] = useState("");
+
+  // handle Function
+  let handleOnClick = (event) => {
+    setName(address);
+    console.log("address set name", address);
   };
+  let handleOnchange = (event) => {
+    setAddress(event.target.value);
+  };
+
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>
-          minh hau code react js with {name} hoc {obj.study}
-        </h2>
+        <h2>minh hau code react js hook {name}</h2>
+        <input
+          type="text"
+          value={address}
+          onChange={(event) => {
+            handleOnchange(event);
+          }}
+        />
+        <button
+          onClick={(event) => {
+            handleOnClick(event);
+          }}
+        >
+          click me
+        </button>
       </header>
     </div>
   );
